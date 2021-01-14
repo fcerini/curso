219ef4d42b888365ed16b51ac9aaa196828c9442
Fernando Cerini <fernando@soflex.com.ar>
2021-01-14 12:47:41 -0300

    productoService handleError

--- pedidos/src/app/shared/producto.service.ts
+++ pedidos/src/app/shared/producto.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 
@@ -16,18 +16,14 @@ export class ProductoService {
     private http: HttpClient
   ) { }
 
-  get(): Observable<any> {
-    return this.http.get(this.url);
+  get(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.url)
+            .pipe(catchError(this.handleError));;
   }
 
 
-
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      return of(result as T);
-    };
+  public handleError(err: Response){
+    return of([]);
   }
 
 }
