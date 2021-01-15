901dc47a51531abcb8745a0d618c9a459983de0a
Fernando Cerini <fernando@soflex.com.ar>
2021-01-15 12:01:51 -0300

    cliente api

---
+++ api/model/cliente.php
@@ -0,0 +1,17 @@
+<?php
+/*
+SELECT [clienId]
+      ,[clienNombre]
+      ,[clienDireccion]
+      ,[clienFechaAlta]
+      ,[clienBorrado]
+  FROM [Curso].[dbo].[Cliente]
+  
+*/
+
+class Cliente
+{
+
+}
+
+?>
